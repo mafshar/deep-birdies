5a10f972fad40e393585e614fe5d482ee0c55bbb
mafshar <ma2510@nyu.edu>
2018-07-07 13:04:12 -0400

    used newer version of Scale -- Resize

--- src/processing/data_processor.py
+++ src/processing/data_processor.py
@@ -2,10 +2,6 @@
 import os
 import sys
 
-import torch
-import torch.nn as nn
-import torch.nn.parallel
-
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
@@ -16,13 +12,13 @@ IMG_SIZE = 64
 
 def get_transformations():
     return transforms.Compose(
-        [transforms.Scale(IMG_SIZE),
+        [transforms.Resize(IMG_SIZE),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 def load_data(data_path):
     try:
-        data = dataset.CIFAR10(
+        data = datasets.CIFAR10(
             root=data_path,
             download=True,
             transform=get_transformations())
