31a667060074f9454e6929717779410b15b3f900
mafshar <ma2510@nyu.edu>
2018-07-07 13:07:32 -0400

    bug fix with weight init

--- src/models/gans.py
+++ src/models/gans.py
@@ -4,7 +4,7 @@ import torch.nn.parallel
 
 def init_weights(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != 01:
+    if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
